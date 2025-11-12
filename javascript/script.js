8f77c961bdf62ba6965cd82d9d310f7897bb86f5
Amity University Tashkent <amity@amity.edu.tas>
2025-11-12 21:48:48 +0500

    1-commit

---
+++ javascript/script.js
@@ -0,0 +1,54 @@
+"use strict";
+// ==========================модификатуры доступа : public, protected, private , readonly
+class PersonalComputer {
+    isRadioMause = true;
+    // mana public qilib qoydim bu bilan hamma joyda ishlatsa boladi
+    MemoryGB = 16;
+    keyboard = "usb";
+    vidioCard = "GeForce RTX 3060 Ti";
+    memorySlots = 5;
+    powerWatt;
+    constructor(powerWatt) {
+        this.powerWatt = powerWatt;
+    }
+    getReadonly() {
+        return this.isRadioMause;
+    }
+    getPrivate() {
+        return this.MemoryGB;
+    }
+    getProtected() {
+        return this.vidioCard;
+    }
+}
+class SystemBlock extends PersonalComputer {
+    powerunit = 1;
+    motherBoard = 1;
+    graphicsCard;
+    number = 4;
+    cdRom;
+    hdd;
+    constructor(powerWatt, cdRom, hdd) {
+        super(powerWatt);
+        this.hdd = hdd;
+        this.cdRom = cdRom;
+        this.vidioCard;
+    }
+}
+const PC = new PersonalComputer(3000);
+const System = new SystemBlock(3000, 8, 4);
+console.log(PC.isRadioMause);
+console.log(System.isRadioMause);
+// public bilan hammasi yaxshi ketyapti
+console.log(PC.MemoryGB);
+console.log(System.getReadonly());
+// readonly da ham muommo yoq lekin readonlyni qiymatini ozgartirib bolmaydi
+// console.log(System.keyboard);
+console.log(PC.getPrivate());
+// 1-sida keyboardni chaqirganda xato berdi private ni faqat oziga tegishli joyda ishlatsa boladi faqat PersonalComputer ichida 
+// console.log(PC.vidioCard);
+console.log(System.getProtected());
+//  vidioCardni extenes qilinganda ham oziga tegishlida ham ishlatsa boladi.
+// ====================  protected bilan private farqi \
+// private faqat  yaratilgan class da bolsa osha yerda ishlaydi protected esa ham osha classda ham extends qilinganda ham ishlaydi
+//# sourceMappingURL=script.js.map
